7049f848342592aeecc60b2d1d3236c1f63c2496
Roman Matsypula <ukrmatsypula@gmail.com>
2021-05-28 22:44:38 +0300

    render card items from vuex

--- src/components/v-catalog.vue
+++ src/components/v-catalog.vue
@@ -3,7 +3,7 @@
     <h1>Catalog</h1>
     <div class="v-catalog__list">
       <v-catalog-item
-        v-for="product in products"
+        v-for="product in PRODUCTS"
         :key="product.article"
         :product_data="product"
         @sendDataToParent="showChildArticleInConsole"
@@ -13,69 +13,30 @@
 </template>
 
 <script>
+import { mapActions, mapGetters } from 'vuex'
 import vCatalogItem from '@/components/v-catalog-item'
 export default {
   name: 'v-catalog',
   components: {
     vCatalogItem,
   },
-  data: () => ({
-    products: [
-      {
-        image: '1.jpg',
-        name: 'T-shirt 1',
-        price: 2100.234234234,
-        article: 'T1',
-        available: true,
-        category: 'Мужские',
-      },
-      {
-        image: '2.jpg',
-        name: 'T-shirt 2',
-        price: 3150.12312412,
-        article: 'T2',
-        available: true,
-        category: 'Женские',
-      },
-      {
-        image: '3.jpg',
-        name: 'T-shirt 3',
-        price: 4200.51524,
-        article: 'T3',
-        available: false,
-        category: 'Женские',
-      },
-      {
-        image: '4.jpg',
-        name: 'T-shirt 4',
-        price: 5300.1245512,
-        article: 'T4',
-        available: true,
-        category: 'Мужские',
-      },
-      {
-        image: '5.jpg',
-        name: 'T-shirt 5',
-        price: 6500.3522314,
-        article: 'T5',
-        available: false,
-        category: 'Женские',
-      },
-      {
-        image: '6.jpeg',
-        name: 'T-shirt 6',
-        price: 8700.4124123,
-        article: 'T6',
-        available: true,
-        category: 'Женские',
-      },
-    ],
-  }),
+  data: () => ({}),
+  computed: {
+    ...mapGetters(['PRODUCTS']),
+  },
   methods: {
+    ...mapActions(['GET_PRODUCTS_FROM_API']),
     showChildArticleInConsole(product) {
       console.log(product)
-    }
-  }
+    },
+  },
+  mounted() {
+    this.GET_PRODUCTS_FROM_API().then(resonse => {
+      if (resonse) {
+        console.log('data arrived')
+      }
+    })
+  },
 }
 </script>
 
--- src/vuex/store.js
+++ src/vuex/store.js
@@ -1,13 +1,38 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
 const store = new Vuex.Store({
-  state: {},
-  getters: {},
-  mutations: {},
-  actions: {},
+  state: {
+    products: [],
+  },
+  getters: {
+    PRODUCTS(state) {
+      return state.products
+    },
+  },
+  mutations: {
+    SET_PRODUCTS_TO_STATE(state, products) {
+      state.products = products
+    },
+  },
+  actions: {
+    async GET_PRODUCTS_FROM_API({ commit }, payload) {
+      return await axios('http://localhost:3000/products', {
+        method: 'GET',
+      })
+        .then(products => {
+          commit('SET_PRODUCTS_TO_STATE', products.data)
+          return products
+        })
+        .catch(err => {
+          console.log(err)
+          return err
+        })
+    },
+  },
 })
 
 export default store
