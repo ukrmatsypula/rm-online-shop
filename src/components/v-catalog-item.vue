6cde5101b6c3d74e5eaf25b3bbbeb6f4fa1384bc
Roman Matsypula <ukrmatsypula@gmail.com>
2021-05-28 21:17:38 +0300

    render card items

--- src/components/v-catalog-item.vue
+++ src/components/v-catalog-item.vue
@@ -1,9 +1,13 @@
 <template>
   <div class="v-catalog-item">
-    <img src="" alt="img">
-    <p>{{ title }}</p>
-    <p>Price: 100</p>
-    <button>add to cart</button>
+    <img
+      class="v-catalog-item__image"
+      :src="require('@/assets/images/' + product_data.image)"
+      :alt="product_data.name"
+    />
+    <p class="v-catalog-item__name">{{ product_data.name }}</p>
+    <p class="v-catalog-item__price">Price: {{ product_data.price}} UAH</p>
+    <button class="v-catalog-item__add-to-cart btn">add to cart</button>
   </div>
 </template>
 
@@ -11,6 +15,12 @@
 export default {
   name: 'v-catalog-item',
   components: {},
+  props: {
+    product_data: {
+      type: Object,
+      require: true,
+    },
+  },
   data: () => ({
     title: 'catalog-item',
   }),
@@ -23,5 +33,9 @@ export default {
   box-shadow: 0 0 8px 0 #e0e0e0;
   padding: $padding * 2;
   margin: $margin * 2;
+
+  &__image {
+    width: 100px;
+  }
 }
 </style>
--- src/components/v-catalog.vue
+++ src/components/v-catalog.vue
@@ -2,7 +2,11 @@
   <div class="v-catalog">
     <h1>Catalog</h1>
     <div class="v-catalog__list">
-      <v-catalog-item />
+      <v-catalog-item
+        v-for="product in products"
+        :key="product.article"
+        :product_data="product"
+      />
     </div>
   </div>
 </template>
@@ -14,7 +18,58 @@ export default {
   components: {
     vCatalogItem,
   },
-  data: () => ({}),
+  data: () => ({
+    products: [
+      {
+        image: '1.jpg',
+        name: 'T-shirt 1',
+        price: 2100.234234234,
+        article: 'T1',
+        available: true,
+        category: 'Мужские',
+      },
+      {
+        image: '2.jpg',
+        name: 'T-shirt 2',
+        price: 3150.12312412,
+        article: 'T2',
+        available: true,
+        category: 'Женские',
+      },
+      {
+        image: '3.jpg',
+        name: 'T-shirt 3',
+        price: 4200.51524,
+        article: 'T3',
+        available: false,
+        category: 'Женские',
+      },
+      {
+        image: '4.jpg',
+        name: 'T-shirt 4',
+        price: 5300.1245512,
+        article: 'T4',
+        available: true,
+        category: 'Мужские',
+      },
+      {
+        image: '5.jpg',
+        name: 'T-shirt 5',
+        price: 6500.3522314,
+        article: 'T5',
+        available: false,
+        category: 'Женские',
+      },
+      {
+        image: '6.jpeg',
+        name: 'T-shirt 6',
+        price: 8700.4124123,
+        article: 'T6',
+        available: true,
+        category: 'Женские',
+      },
+    ],
+  }),
 }
 </script>
 
