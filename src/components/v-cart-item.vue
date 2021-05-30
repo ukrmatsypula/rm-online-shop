622163cb2cf8f236db74b98f8b542eda467df026
Roman Matsypula <ukrmatsypula@gmail.com>
2021-05-30 14:38:39 +0300

    create and required cart-item component

---
+++ src/components/v-cart-item.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="v-cart-item">
+    <p>cart-item</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'v-cart-item'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/components/v-cart.vue
+++ src/components/v-cart.vue
@@ -1,13 +1,18 @@
 <template>
   <div class="v-cart">
     <p>{{ title }}</p>
+    <v-cart-item />
   </div>
 </template>
 
 <script>
+import vCartItem from '@/components/v-cart-item'
+
 export default {
   name: 'v-cart',
-  components: {},
+  components: {
+    vCartItem,
+  },
   data: () => ({
     title: 'Cart',
   }),
